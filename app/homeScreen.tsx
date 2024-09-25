8d40718c559db1d2cbfc728520376ebdfea43bbb
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-09-25 19:56:02 +0530

    fix: type fix in homeScreen

--- app/homeScreen.tsx
+++ app/homeScreen.tsx
@@ -14,6 +14,21 @@ import { searchSongs } from "../utils/api";
 import { RootState } from "../redux/store";
 import { useRouter } from "expo-router";
 
+interface Song {
+  trackId: any;
+  artistName: string;
+  collectionName: string;
+  trackName: string;
+  artworkUrl100: string;
+  trackPrice: number;
+  releaseDate: string;
+  genre: string;
+  country: string;
+  description: string;
+  kind: string;
+  longDescription: string;
+}
+
 const HomeScreen = () => {
   const dispatch = useDispatch();
   const { songList } = useSelector((state: RootState) => state.songs);
@@ -39,7 +54,7 @@ const HomeScreen = () => {
     setFilteredSongs(filteredData);
   }, [searchTerm, songList]);
 
-  const handlePress = (song: any) => {
+  const handlePress = (song: Song) => {
     console.log(song);
     dispatch(setSelectedSong(song));
     router.push("/details");
@@ -49,7 +64,7 @@ const HomeScreen = () => {
     <View style={styles.container}>
       <TextInput
         style={styles.searchBar}
-        placeholder="Search songs or artists"
+        placeholder="Search songs"
         value={searchTerm}
         onChangeText={setSearchTerm}
         placeholderTextColor={"white"}
@@ -134,99 +149,3 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen;
-
-// import React, { useEffect } from "react";
-// import {
-//   View,
-//   FlatList,
-//   Text,
-//   Image,
-//   TouchableOpacity,
-//   StyleSheet,
-// } from "react-native";
-
-// import { useDispatch, useSelector } from "react-redux";
-// import { setSongList, setSelectedSong } from "../redux/slices/songSlice";
-// import { searchSongs } from "../utils/api";
-// import { RootState } from "../redux/store";
-// import { useRouter } from "expo-router";
-
-// const HomeScreen = () => {
-//   const dispatch = useDispatch();
-//   const { songList } = useSelector((state: RootState) => state.songs);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     searchSongs("jack johnson")
-//       .then((response) => {
-//         dispatch(setSongList(response.data.results));
-//       })
-//       .catch((error) => console.error(error));
-//   }, [dispatch]);
-
-//   const handlePress = (song: any) => {
-//     dispatch(setSelectedSong(song));
-//     router.push("/details");
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <FlatList
-//         data={songList}
-//         keyExtractor={(item) => item.trackId.toString()}
-//         renderItem={({ item }) => (
-//           <TouchableOpacity onPress={() => handlePress(item)}>
-//             <View style={{ flexDirection: "row", padding: 10 }}>
-//               <Image
-//                 source={{ uri: item.artworkUrl100 }}
-//                 style={{ width: 50, height: 50 }}
-//               />
-//               <View style={styles.textContainer}>
-//                 <Text style={styles.text1}>{item.trackName}</Text>
-//                 <View style={styles.textView}>
-//                   <Text style={styles.text2}>{item.artistName}</Text>
-//                   <Text style={styles.text3}>
-//                     {" "}
-//                     {item.collectionName ? "\u2022 " : ""} {item.collectionName}
-//                   </Text>
-//                 </View>
-//               </View>
-//             </View>
-//           </TouchableOpacity>
-//         )}
-//       />
-//     </View>
-//   );
-// };
-
-// export default HomeScreen;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#111111",
-//   },
-
-//   textContainer: {
-//     marginLeft: 1,
-//     color: "white",
-//   },
-//   text1: {
-//     color: "white",
-//     marginLeft: 8,
-//   },
-//   text2: {
-//     color: "white",
-//     marginLeft: 8,
-//     fontSize: 12,
-//   },
-//   text3: {
-//     color: "white",
-//     marginLeft: 8,
-//     fontSize: 12,
-//     width: "80%",
-//   },
-//   textView: {
-//     flexDirection: "row",
-//   },
-// });
